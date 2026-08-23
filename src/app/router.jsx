21779564f241be1bd9fb21733c736fd5834314fe
Rasibul <rasibul179@gmail.com>
2026-08-23 22:27:23 +0600

    setup online-ticket-web

---
+++ src/app/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Online Ticket Booking</h1>,
+  },
+]);
+
+export default router;
