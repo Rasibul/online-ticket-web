3a11037578b88a963ae7562516d7fb02b223828a
Rasibul <rasibul179@gmail.com>
2026-08-25 07:09:35 +0600

    add RegisterPage component for user registration

---
+++ src/feature/auth/pages/RegisterPage.jsx
@@ -0,0 +1,28 @@
+import RegisterForm 
+from "../components/RegisterForm";
+
+
+const RegisterPage=()=>{
+
+
+return (
+
+<div>
+
+<h1>
+Create Account
+</h1>
+
+
+<RegisterForm/>
+
+
+</div>
+
+);
+
+
+};
+
+
+export default RegisterPage;
