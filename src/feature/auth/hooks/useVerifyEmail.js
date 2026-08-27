a68be8a4c07b8282ca13c031c8108813a3e376db
Rasibul <rasibul179@gmail.com>
2026-08-27 20:23:25 +0600

    feat: add useVerifyEmail hook for email verification functionality

---
+++ src/feature/auth/hooks/useVerifyEmail.js
@@ -0,0 +1,24 @@
+import {
+useMutation
+} from "@tanstack/react-query";
+
+
+import {
+verifyEmail
+} from "../api/authApi";
+
+
+
+export const useVerifyEmail = ()=>{
+
+
+return useMutation({
+
+mutationFn:
+verifyEmail
+
+
+});
+
+
+};
